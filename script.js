48e554d4e0b3bf0c4cd987c21070c7966aba6925
abdullahamir1233 <bscs21042@itu.edu.pk>
2024-09-21 21:20:22 +0500

    made a json object

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// function googleTranslateElementInit() {
-//     new google.translate.TranslateElement({
-//         pageLanguage: 'en', // Set the default language of your page
-//         includedLanguages: 'en,es,fr,de,zh', // List of languages you want to support
-//         layout: google.translate.TranslateElement.InlineLayout.SIMPLE
-//     }, 'google_translate_element');
-// }
-
-// // Load the Google Translate API script and initialize the widget
-// (function() {
-//     var script = document.createElement('script');
-//     script.type = 'text/javascript';
-//     script.src = 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
-//     document.head.appendChild(script);
-// })();
 function googleTranslateElementInit() {
     new google.translate.TranslateElement({
         pageLanguage: 'en', // Set the default language of your page
